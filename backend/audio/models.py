039e5a3f59eebfe8ec0d576ffe24b880a8beff0b
gkgkgkgkdlel <gkgkgkgkdlel@naver.com>
2022-09-29 23:54:40 +0900

    Fix: project 모델을 audio에 옮기고 모델들의 컬럼 변경

--- backend/audio/models.py
+++ backend/audio/models.py
@@ -1,20 +1,37 @@
 from cgitb import text
 from turtle import speed
 from django.db import models
-from project.models import Project
+
 
 # Create your models here.
+
+
+class Project(models.Model):
+    # index = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
+    project_title = models.CharField(max_length=100)
+    update_time = models.DateTimeField(auto_now=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+
 class Audio(models.Model):
     # index = models.AutoField(primary_key=True)
     id = models.AutoField(primary_key=True)
     audio_file = models.FileField(verbose_name="오디오 파일명", null=True)
-    speed = models.FloatField(max_length=10)
-    update_time = models.DateTimeField(auto_now=True)
-    speed = models.IntegerField(default=0)
+    speed = models.FloatField(max_length=10, default=1.0)
     project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    update_time = models.DateTimeField(auto_now=True)
 
 
 class Text(models.Model):
-    text = models.CharField(max_length=300)
+    id = models.AutoField(primary_key=True)
+    text = models.TextField()
     idx = models.IntegerField(verbose_name="텍스트 인덱스")
     audio_id = models.ForeignKey(Audio, on_delete=models.CASCADE)
+
+
+class TextList(models.Model):
+    id = models.AutoField(primary_key=True)
+    sentence_idx = models.IntegerField(verbose_name="문장 인덱스", default=0)
+    text_id = models.ForeignKey(Text, on_delete=models.CASCADE)
+    sentence = models.TextField()
--- backend/project/models.py
+++ backend/project/models.py
@@ -2,9 +2,3 @@ from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
-class Project(models.Model):
-    # index = models.AutoField(primary_key=True)
-    project_id = models.AutoField(primary_key=True)
-    project_title = models.CharField(max_length=100)
-    update_time = models.DateTimeField(auto_now=True)
-    create_time = models.DateTimeField(auto_now_add=True)
